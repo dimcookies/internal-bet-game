6a9504d7acd93d7a48e5cc1c2fff66883c8f87fa
George Karoulas <george.karoulas@upstreamsystems.com>
2018-06-06 20:24:42 +0300

    home page fixes

--- src/main/worldcup2018/src/app/home/home.controller.js
+++ src/main/worldcup2018/src/app/home/home.controller.js
@@ -1,40 +1,11 @@
 ﻿export default class HomeController {
     constructor($rootScope, $scope, NgTableParams, $http, $timeout, logger) {
-        // console.log('angulardataTables  ', angulardataTables);
         this.$rootScope = $rootScope;
         this.$http = $http;
         this.$timeout = $timeout;
         this.messages = [];
         this.logger = logger;
         this.NgTableParams = NgTableParams;
-
-        // this.options = {
-        //   scrollbarV: false
-        // };
-
-        // this.allLeaders  = [
-        //   { name: 'alpha', points: 500, prize: 500},
-        //   { name: 'beta', points: 1500, prize: 500},
-        //   { name: 'gama', points: 800, prize: 500}
-        // ];
-        // self.allLeaders = [
-        //     [
-        //         "Tiger Nixon",
-        //         "System Architect",
-        //         "Edinburgh",
-        //         "5421",
-        //         "2011\/04\/25",
-        //         "$320,800"
-        //     ],
-        //     [
-        //         "Tiger Nixon",
-        //         "System Architect",
-        //         "Edinburgh",
-        //         "5421",
-        //         "2011\/04\/25",
-        //         "$320,800"
-        //     ]
-        // ]
         this.activate();
     }
     fetchData() {
@@ -44,58 +15,56 @@
         });
         self.$http.get("/comments/list?limit=5").then(function(response) {
             self.allComments = response.data;
-            //             $('#example').DataTable( {
-            //     data: response.data
-            // } );
-            // self.allLeaders = [{
-            //     name: 'alpha',
-            //     points: 500,
-            //     prize: 500
-            // }, {
-            //     name: 'beta',
-            //     points: 1500,
-            //     prize: 500
-            // }, {
-            //     name: 'gama',
-            //     points: 800,
-            //     prize: 500
-            // }];
-            // $('#example').DataTable({
-            //     "paging": false,
-            //     "ordering": false,
-            //     "info": false
-            // })
         });
-        // var vm = this;
-        //  vm.dtOptions = DTOptionsBuilder.fromFnPromise(function() {
-        //      var defer = $q.defer();
-        //      $http.get("/comments/list?limit=5").then(function(result) {
-        //          defer.resolve(result.data);
-        //      });
-        //      return defer.promise;
-        //  }).withPaginationType('full_numbers');
-
-        //  vm.dtColumns = [
-        //      DTColumnBuilder.newColumn('id').withTitle('ID'),
-        //      DTColumnBuilder.newColumn('firstName').withTitle('First name'),
-        //      DTColumnBuilder.newColumn('lastName').withTitle('Last name').notVisible()
-        //  ];       
         var self = this;
-        var data = [
-            { name: "Moroni", age: 50 },
-            { name: "Tiancum", age: 43 },
-            { name: "Jacob", age: 27 },
-            { name: "Nephi", age: 29 },
-            { name: "Enos", age: 34 },
-            { name: "Tiancum", age: 43 },
-            { name: "Jacob", age: 27 },
-            { name: "Nephi", age: 29 },
-            { name: "Enos", age: 34 },
-            { name: "Tiancum", age: 43 }];
-        self.tableParams = new self.NgTableParams({
-            sorting: { name: "asc" } 
+        var data = [{
+            name: 'alpha',
+            points: 500,
+            prize: 500
         }, {
-            dataset: data
+            name: 'beta',
+            points: 1500,
+            prize: 500
+        }, {
+            name: 'gama',
+            points: 800,
+            prize: 500
+        }, {
+            name: 'gama',
+            points: 800,
+            prize: 500
+        }, {
+            name: 'gama',
+            points: 800,
+            prize: 500
+        }, {
+            name: 'gama',
+            points: 800,
+            prize: 500
+        }, {
+            name: 'gama',
+            points: 800,
+            prize: 500
+        }, {
+            name: 'gama',
+            points: 800,
+            prize: 500
+        }, {
+            name: 'gama',
+            points: 800,
+            prize: 500
+        }, {
+            name: 'gama',
+            points: 800,
+            prize: 500
+}];
+
+        self.tableParams = new self.NgTableParams({
+            count: data.length // hides pager
+        }, {
+            dataset: data,
+            total:1,
+            counts: [] // hides page sizes
         });
     }
     activate() {
