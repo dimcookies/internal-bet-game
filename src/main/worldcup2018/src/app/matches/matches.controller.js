77e3b3544d20fa0d666f4fe095cd4d1e9f0a93eb
George Karoulas <george.karoulas@upstreamsystems.com>
2018-06-06 20:53:01 +0300

    matches datatble + leaderboard

--- src/main/worldcup2018/src/app/matches/matches.controller.js
+++ src/main/worldcup2018/src/app/matches/matches.controller.js
@@ -1,19 +1,22 @@
 ﻿export default class MatchesController {
-    constructor($scope, $http, logger) {
-
+    constructor($scope, $http, NgTableParams, logger) {
         this.$http = $http;
         this.messages = [];
         this.logger = logger;
-
+        this.NgTableParams = NgTableParams;
         this.activate();
     }
 
     activate() {
         var self = this;
-            self.$http.get("/games/list").then(function (response) {
-                self.allMatches = response.data;
-           });
+        self.$http.get("/games/list").then(function(response) {
+            self.tableParams = new self.NgTableParams({
+                count: response.data.length // hides pager
+            }, {
+                dataset: response.data,
+            });
+        });
     }
 }
 
-MatchesController.$inject = ['$scope', '$http', 'logger'];
+MatchesController.$inject = ['$scope', '$http', 'NgTableParams', 'logger'];
