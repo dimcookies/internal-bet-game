efa365ef47444a12ae41fac805a785f4aa4252cc
George Karoulas <george.karoulas@upstreamsystems.com>
2018-06-06 13:36:40 +0300

    fronend fixes

--- src/main/worldcup2018/src/app/home/home.controller.js
+++ src/main/worldcup2018/src/app/home/home.controller.js
@@ -1,18 +1,31 @@
 ﻿export default class HomeController {
-    constructor($scope, $http, logger) {
+    constructor($rootScope, $scope, $http, $timeout, logger) {
+        this.$rootScope = $rootScope;
         this.$http = $http;
+        this.$timeout = $timeout;
         this.messages = [];
         this.logger = logger;
         this.activate();
     }
-
-
-    activate() {
+    fetchData() {
         var self = this;
         self.$http.get("/rss/list").then(function(response) {
             self.allRss = response.data;
         });
+        self.$http.get("/comments/list?limit=5").then(function(response) {
+            self.allComments = response.data;
+        });
+    }
+    activate() {
+        var self = this;
+        self.fetchData();
+        self.mytimeout = self.$timeout(function() {
+            self.activate();
+        }, 5000);
+        this.$rootScope.$on('$locationChangeStart', function() {
+            self.$timeout.cancel(self.mytimeout);
+        });
     }
 }
 
-HomeController.$inject = ['$scope', '$http', 'logger'];
+HomeController.$inject = ['$rootScope', '$scope', '$http', '$timeout', 'logger'];
