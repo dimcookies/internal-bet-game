7b592095728466dcd7151bc148dca629824805ad
dimcookies <dimcookies@gmail.com>
2018-06-10 10:32:24 +0300

    Fix rss urls. Remove invalid characters

--- src/main/java/bet/service/rss/RssFeedScheduler.java
+++ src/main/java/bet/service/rss/RssFeedScheduler.java
@@ -6,17 +6,21 @@ import com.sun.syndication.feed.synd.SyndEnclosure;
 import com.sun.syndication.feed.synd.SyndEntry;
 import com.sun.syndication.feed.synd.SyndFeed;
 import com.sun.syndication.io.SyndFeedInput;
-import com.sun.syndication.io.XmlReader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.io.Reader;
+import java.io.StringReader;
 import java.util.Arrays;
 
 /**
@@ -34,26 +38,26 @@ public class RssFeedScheduler {
 	@Autowired
 	private RssFeedRepository rssFeedRepository;
 
+	@Autowired
+	private RestTemplate restTemplate;
+
 	@Scheduled(fixedRate = 3600000)
 	public void getRssFeed() {
 		//delete existing records
 		rssFeedRepository.deleteAll();
 
-		Arrays.stream(feeds).map(feed -> {
-			try {
-				return new URL(feed);
-			} catch (MalformedURLException e) {
-				throw new RuntimeException(e);
-			}
-		}).forEach(feedUrl -> {
+		Arrays.stream(feeds).forEach(feedUrl -> {
 			try {
 				SyndFeedInput input = new SyndFeedInput();
-				SyndFeed feed = input.build(new XmlReader(feedUrl));
+				input.setXmlHealerOn(true);
+
+				//SyndFeed feed = input.build(new XmlReader(feedUrl));
+				SyndFeed feed = input.build(getResponse(feedUrl));
 				feed.getEntries().forEach (e -> {
 					SyndEntry entry = (SyndEntry) e;
 					//get image if exist
 					String imageUrl = entry.getEnclosures() != null && entry.getEnclosures().size() > 0 ? ((SyndEnclosure)entry.getEnclosures().get(0)).getUrl().replaceFirst("^//","http://") : "/images/emptyRss.jpg";
-					rssFeedRepository.save(new RssFeed(cleanTextContent(entry.getTitle()), entry.getLink(),  entry.getPublishedDate(), imageUrl));
+					rssFeedRepository.save(new RssFeed(entry.getTitle(), entry.getLink(), entry.getPublishedDate(), imageUrl));
 				});
 			} catch (Exception e) {
 				LOGGER.error("Error loading rss " + feedUrl, e);
@@ -74,6 +78,19 @@ public class RssFeedScheduler {
 		text = text.replaceAll("\\p{C}", "");
 
 		return text.trim();
+	}
+
+	private Reader getResponse(String feedUrl) {
+		HttpHeaders headers = new HttpHeaders();
+		//headers.add("Content-Type", "application/json");
+		headers.add("Accept", "*/*");
+
+		HttpEntity<String> requestEntity = new HttpEntity<>("", headers);
+		ResponseEntity<String> responseEntity = restTemplate.exchange(feedUrl, HttpMethod.GET, requestEntity, String.class);
+		String response = cleanTextContent(responseEntity.getBody());
+
+		return new StringReader(response);
+
 	}
 
 
