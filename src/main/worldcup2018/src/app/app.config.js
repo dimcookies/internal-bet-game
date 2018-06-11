201e17908ef308ad9028f21ffa3d5686457aac26
George Karoulas <george.karoulas@upstreamsystems.com>
2018-06-11 16:05:55 +0300

    polling fixes for currentUser

--- src/main/worldcup2018/src/app/app.config.js
+++ src/main/worldcup2018/src/app/app.config.js
@@ -3,8 +3,7 @@
 export default  function config($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
     $locationProvider.hashPrefix('!');
     $urlRouterProvider.otherwise('/');
-
-//    $httpProvider.interceptors.push(seoInterceptor);
+	//$httpProvider.interceptors.push(seoInterceptor);
 }
 
 
--- src/main/worldcup2018/src/app/layout/layout.controller.js
+++ src/main/worldcup2018/src/app/layout/layout.controller.js
@@ -1,17 +1,35 @@
 ﻿import angular from 'angular';
 
 export default class LayoutController {
-	    // constructor() {
-    constructor($scope) {
-        this.pageTitle = 'AngularJS + ES6 application using Webpack';
-        // this.template = '<div id="layout"></div>';
+    // constructor() {
+    constructor($scope, $http, $timeout, $window) {
+        this.$http = $http;
+        this.$window = $window;
+        this.$timeout = $timeout;
+        this.pollUser();
+        var self = this;
+        self.pollUser();
 
-        $scope.$on('$stateChangeSuccess', (event, toState, toParams, fromState, fromParams)=> {
-            if (angular.isDefined(toState.data.pageTitle)) {
-                this.pageTitle = `${toState.data.pageTitle} | AngularJS + ES6 application using Webpack`;
-            }
+        $scope.$on('$stateChangeSuccess', (event, toState, toParams, fromState, fromParams) => {
+            self.pollUser();
         });
+    }
+    pollUser() {
+        var self = this;
+        if(self.mytimeoutPoll){
+            self.$timeout.cancel(self.mytimeoutPoll);            
+        }
+
+        self.mytimeoutPoll = self.$timeout(function() {
+            self.$http.get("/users/currentUser").then(function(response) {
+                // console.log('response    ', response);
+            }).catch(function(data) {
+                self.$window.location.reload();
+                // Handle error here
+            });
+
+        }, 90000);
     }
 }
 
-LayoutController.$inject = ['$scope'];
+LayoutController.$inject = ['$scope', '$http', '$timeout', '$window'];
