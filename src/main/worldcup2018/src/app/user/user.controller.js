e11166a62e8a70d236f33314734346532220a085
George Karoulas <george.karoulas@upstreamsystems.com>
2018-06-13 15:50:36 +0300

    added menu side

--- src/main/worldcup2018/src/app/user/user.controller.js
+++ src/main/worldcup2018/src/app/user/user.controller.js
@@ -2,13 +2,14 @@
     constructor($scope, $stateParams, $http, NgTableParams, logger) {
         this.$http = $http;
         this.logger = logger;
-        this.NgTableParams = NgTableParams;                
+        this.NgTableParams = NgTableParams;
         this.userName = $stateParams.userName; //getting userName
         this.activate();
-    }
 
-    activate() {
+    }
+    fetchBets() {
         var self = this;
+
         this.$http.get("/bets/list?userName=" + self.userName).then(function(response) {
             // self.selectedBets = response.data;
             self.selectedUser = self.userName;
@@ -16,11 +17,24 @@
                 count: response.data.length // hides pager
             }, {
                 dataset: response.data,
-                total:1,
+                total: 1,
                 counts: [] // hides page sizes                
-            });            
+            });
         });
+
+    }
+    activate() {
+        var self = this;
+        if (this.userName) {
+            fetchBets();
+        } else {
+            this.$http.get("/users/currentUser").then(function(response) {
+                // console.log('response///...  ', response);
+                self.userName = response.data.username
+                self.fetchBets();
+            });
+        }
     }
 }
 
-UserController.$inject = ['$scope', '$stateParams', '$http','NgTableParams', 'logger'];
+UserController.$inject = ['$scope', '$stateParams', '$http', 'NgTableParams', 'logger'];
